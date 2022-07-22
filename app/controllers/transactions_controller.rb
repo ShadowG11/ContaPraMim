04fcdbbcdb0ebb5575a546978d9c28e6f17db1bd
ShadowG11 <gersonfmfilho@hotmail.com>
2022-07-22 16:58:02 -0300

    implemented show, create and transaction_params functions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,23 @@
 class TransactionsController < ApplicationController
+  def show
+    @transaction = Transaction.find(params[:id])
+  end
+
   def new
     @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+
+    if @transaction.save
+      redirect_to @transaction
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:title, :value, :type, :date)
   end
 end
