20bfa11edce38f24d284ee088c187b9b1424fb45
ShadowG11 <gersonfmfilho@hotmail.com>
2022-07-25 10:47:13 -0300

    added validation on transaction columns

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,7 @@
 class Transaction < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 2}
+  validates :value, presence: true
+  validates :category, presence: true
+  validates :date
+  validates :description
 end
