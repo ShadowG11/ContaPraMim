bd268e60a7eb5edd988fd65fd099f42fa2dad242
ShadowG11 <gersonfmfilho@hotmail.com>
2022-07-25 15:37:06 -0300

    show money statistics (money gained, spent and total balance) in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
     @transactions = Transaction.all
+    @incomings = Transaction.where(category: 'income').sum(:value)
+    @spendings = Transaction.where(category: 'spending').sum(:value)
+    @total_money = @incomings - @spendings
   end
 end
