f70d1dab045dc3e04dd75188b5c948f4d09bc49c
ShadowG11 <gersonfmfilho@hotmail.com>
2022-07-25 10:15:50 -0300

    added edit, update and destroy function and added description to filtered params

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -17,7 +17,28 @@ class TransactionsController < ApplicationController
     end
   end
 
+  def edit
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def update
+    @transaction = Transaction.find(params[:id])
+
+    if @transaction.update(transaction_params)
+      redirect_to @transaction
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @transaction = Transaction.find(params[:id])
+    @transaction.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
   def transaction_params
-    params.require(:transaction).permit(:title, :value, :category, :date)
+    params.require(:transaction).permit(:title, :value, :category, :date, :description)
   end
 end
