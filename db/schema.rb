cd1b72e04f5ad46aa53f29cc9fdb8d589a3934a5
ShadowG11 <gersonfmfilho@hotmail.com>
2022-08-03 11:07:29 -0300

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_03_133733) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_03_140207) do
   create_table "transactions", force: :cascade do |t|
     t.string "title"
     t.decimal "value"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_03_133733) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
