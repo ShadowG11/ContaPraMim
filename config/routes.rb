9316afb45206678f0ea04019703ed21edef6b789
ShadowG11 <gersonfmfilho@hotmail.com>
2022-08-03 12:13:39 -0300

    update routes.rb to all actions for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "home#index"
+  resources :users
   resources :transactions
 end
