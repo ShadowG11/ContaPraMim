ba7636f4a49c815d214fad058b2158f570bc54f6
ShadowG11 <gersonfmfilho@hotmail.com>
2022-08-03 17:00:27 -0300

    add sign in and out routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   root "home#index"
   resources :users
   resources :transactions
-  get 'sessions/new'
+  get    'sign_in'  => 'sessions#new'
+  post   'sign_in'  => 'sessions#create'
+  delete 'sign_out' => 'sessions#destroy'
 end
