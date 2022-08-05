f30a9560a51a4ec477f1c65872f0110842e3f9f0
ShadowG11 <gersonfmfilho@hotmail.com>
2022-08-05 16:22:49 -0300

    fix create function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      sign_in @user
-    end
-    redirect_to @user
+      sign_in(@user)
+      redirect_to current_user
     else
-      render 'new'
+      render action: :new
+    end
   end
 
   def destroy
