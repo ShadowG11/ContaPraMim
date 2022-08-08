b5676180742c7965a8df8aad560babc3651cf319
ShadowG11 <gersonfmfilho@hotmail.com>
2022-08-08 11:12:30 -0300

    add edit, update and destroy actions in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to users_path
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    sign_out
+    redirect_to root_path
+  end
 
 private
   def user_params
